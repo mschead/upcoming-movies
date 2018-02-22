184b3d11fe167b30411ba755a5abb1535b17d3a4
Marcos Schead <marcos@bridge.ufsc.br>
2018-02-22 18:42:29 -0300

    Creating the movies collection view: using a mock image whereas there's no real movies

---
+++ upcoming-movies/MovieCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  MovieCollectionViewCollectionViewCell.swift
+//  upcoming-movies
+//
+//  Created by Marcos Schead on 22/02/18.
+//  Copyright © 2018 Marcos Schead. All rights reserved.
+//
+
+import UIKit
+
+class MovieCollectionViewCell: UICollectionViewCell {
+
+    @IBOutlet weak var movieImage: UIImageView!
+
+}
--- upcoming-movies/ViewController.swift
+++ upcoming-movies/ViewController.swift
@@ -8,18 +8,45 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UICollectionViewDelegateFlowLayout, UICollectionViewDataSource {
+
+    @IBOutlet weak var moviesCollectionView: UICollectionView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        self.moviesCollectionView.delegate = self
+        self.moviesCollectionView.dataSource = self
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 20
     }
 
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "movieCollectionCell", for: indexPath) as! MovieCollectionViewCell
+        cell.movieImage.image = UIImage(named: "lego-poster")
+        return cell
+    }
+
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let width  = self.view.frame.size.width;
+        let insetLeft = collectionViewLayout.collectionView?.layoutMargins.left ?? 0
+        let insetRight = collectionViewLayout.collectionView?.layoutMargins.right ?? 0
+
+        let widthCell = width * 0.32
+        let totalWidth = widthCell * 3 + insetLeft + insetRight + 5 + 5
+
+        if (totalWidth < width) {
+            return CGSize(width: width * 0.32, height: width * 0.32 * 1.54)
+        } else {
+            return CGSize(width: width * 0.3, height: width * 0.3 * 1.54)
+        }
+    }
+
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return UIEdgeInsetsMake(0, 0, 0, 0);
+    }
 
 }
 
