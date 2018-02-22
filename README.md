16054c443722ba6c51d2a1ea1f9294991b0d9430
Marcos Schead <marcos@bridge.ufsc.br>
2018-02-22 14:49:32 -0300

    Initializing Repo: initial code project and README

---
+++ README.md
@@ -0,0 +1,19 @@
+# Upcoming Movies Challenge
+
+### Development Purpose
+
+* Simple iOS App, that you can check a list of incoming movies and their details.
+* Built to try an iOS Developer Position.
+
+### Instructions
+
+1. This project uses CocoaPods to manage the project dependencies. Access https://cocoapods.org/ and follow
+the install part, only. The remaining parts will be explained better here.
+
+2. Clone this repository.
+
+3. Access the project folder through the terminal, where you can find the _Podfile_.
+
+4. Type the command **pod update** and wait some seconds until the dependencies get installed.
+
+5. Then, to import the project in Xcode, you must open the file **articles-app.xcworkspace**, to allow Cocoapods manage the dependencies.
---
+++ upcoming-moviesTests/upcoming_moviesTests.swift
@@ -0,0 +1,36 @@
+//
+//  upcoming_moviesTests.swift
+//  upcoming-moviesTests
+//
+//  Created by Marcos Schead on 22/02/18.
+//  Copyright © 2018 Marcos Schead. All rights reserved.
+//
+
+import XCTest
+@testable import upcoming_movies
+
+class upcoming_moviesTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
